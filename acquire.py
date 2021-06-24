84c90ab5f5dd46fdd9e695043fbf7457c534723f
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-06-23 20:56:21 -0500

    update script

---
+++ acquire.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import os
+# use get_db_url function to connect to the codeup db
+from env import get_db_url
+
+
+def get_zillow_data(cached=False):
+    '''
+    This function returns the zillow database as a pandas dataframe. 
+    If the data is cached or the file exists in the directory, the function will read the data into a df and return it. 
+    Otherwise, the function will read the database into a dataframe, cache it as a csv file
+    and return the dataframe.
+    '''
+    # If the cached parameter is false, or the csv file is not on disk, read from the database into a dataframe
+    if cached == False or os.path.isfile('zillow_df.csv') == False:
+        sql_query = '''
+        SELECT * 
+        FROM predictions_2017 pred
+        LEFT JOIN properties_2017 USING(parcelid)
+        LEFT JOIN airconditioningtype USING(airconditioningtypeid)
+        LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)
+        LEFT JOIN buildingclasstype USING(buildingclasstypeid)
+        LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)
+        LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+        LEFT JOIN storytype USING(storytypeid)
+        LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)
+        WHERE pred.transactiondate LIKE '2017%'
+        AND latitude IS NOT NULL
+        AND longitude IS NOT NULL
+        AND propertylandusetypeid IN (261, 263, 264, 265, 266, 268, 273, 275, 276, 279);  
+        '''
+        zillow_df = pd.read_sql(sql_query, get_db_url('zillow'))
+        #also cache the data we read from the db, to a file on disk
+        zillow_df.to_csv('zillow_df.csv')
+    else:
+        # either the cached parameter was true, or a file exists on disk. Read that into a df instead of going to the database
+        zillow_df = pd.read_csv('zillow_df.csv', index_col=0)
+    # return our dataframe regardless of its origin
+    return zillow_df
+
+
+def summarize(df):
+    '''
+    This function will take in a single argument (a pandas dataframe) and 
+    output to console various statistices on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
+    '''
+    print('----------------------')
+    print('Dataframe head')
+    print(df.head(3))
+    print('----------------------')
+    print('Dataframe Info ')
+    print(df.info())
+    print('----------------------')
+    print('Dataframe Description')
+    print(df.describe())
+    print('----------------------')
+    num_cols = [col for col in df.columns if df[col].dtype != 'object']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('----------------------')
+    print('Dataframe value counts ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            # define bins for continuous columns and don't sort them
+            print(df[col].value_counts(bins=10, sort=False))
+    print('----------------------')
+    
+
+
